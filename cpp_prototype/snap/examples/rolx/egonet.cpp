e2de9ef5b05fdc0c86042774150b8a20d7c7900a
guotai <taig@stanford.edu>
2014-02-22 15:59:15 -0800

    add directed egonet

--- cpp_prototype/snap/examples/rolx/egonet.cpp
+++ cpp_prototype/snap/examples/rolx/egonet.cpp
@@ -2,11 +2,11 @@
 
 TUNEgonet::TUNEgonet(const PUNGraph& Graph, const int CtrNId) {
   this->AddNode(CtrNId);
+  this->CtrNId = CtrNId;
   const TUNGraph::TNodeI& CtrNode = Graph->GetNI(CtrNId);
   for (int i = 0; i < CtrNode.GetInDeg(); ++i) {
     this->AddNode(CtrNode.GetInNId(i));
   }
-  this->CtrNId = CtrNId;
   this->ArndEdges = 0;
   for (int i = 0; i < CtrNode.GetInDeg(); ++i) {
     int NbrNId = CtrNode.GetInNId(i);
@@ -17,10 +17,42 @@ TUNEgonet::TUNEgonet(const PUNGraph& Graph, const int CtrNId) {
         if (!this->IsEdge(NbrNId, NbrNbrNId)) {
           this->AddEdge(NbrNId, NbrNbrNId);
         }
-      } else {
+      }
+      else {
         this->ArndEdges++;
       }
     }
-    this->WthnEdges = this->GetEdges();
   }
+  this->WthnEdges = this->GetEdges();
+}
+
+TNEgonet::TNEgonet(const PNGraph& Graph, const int CtrNId) {
+  this->AddNode(CtrNId);
+  this->CtrNId = CtrNId;
+  const TNGraph::TNodeI& CtrNode = Graph->GetNI(CtrNId);
+  for (int i = 0; i < CtrNode.GetDeg(); ++i) {
+    this->AddNode(CtrNode.GetNbrNId(i));
+  }
+  this->InEdges = 0;
+  this->OutEdges = 0;
+  for (int i = 0; i < CtrNode.GetDeg(); ++i) {
+    int NbrNId = CtrNode.GetNbrNId(i);
+    const TNGraph::TNodeI& NbrNode = Graph->GetNI(NbrNId);
+    for (int j = 0; j < NbrNode.GetInDeg(); ++j) {
+      int NbrNbrNId = NbrNode.GetInNId(j);
+      if (this->IsNode(NbrNbrNId)) {
+        this->AddEdge(NbrNbrNId, NbrNId);
+      }
+      else {
+        this->InEdges++;
+      }
+    }
+    for (int j = 0; j < NbrNode.GetOutDeg(); ++j) {
+      int NbrNbrNId = NbrNode.GetOutNId(j);
+      if (!this->IsNode(NbrNbrNId)) {
+        this->OutEdges++;
+      }
+    }
+  }
+  this->WthnEdges = this->GetEdges();
 }
--- cpp_prototype/snap/examples/rolx/egonet.h
+++ cpp_prototype/snap/examples/rolx/egonet.h
@@ -4,16 +4,30 @@
 #include "stdafx.h"
 
 class TUNEgonet;
+class TNEgonet;
 typedef TPt<TUNEgonet> PUNEgonet;
+typedef TPt<TNEgonet> PNEgonet;
 
 class TUNEgonet : public TUNGraph {
 private:
-  TInt WthnEdges, ArndEdges;
   TInt CtrNId;
+  TInt WthnEdges, ArndEdges;
 public:
-  TUNEgonet(const PUNGraph& Graph, const int CenterNode);
+  TUNEgonet(const PUNGraph& Graph, const int CtrNId);
   int GetWthnEdges() { return WthnEdges; }
   int GetArndEdges() { return ArndEdges; }
+  int GetCtrNId() { return CtrNId; }
+};
+
+class TNEgonet : public TNGraph {
+private:
+  TInt CtrNId;
+  TInt WthnEdges, InEdges, OutEdges;
+public:
+  TNEgonet(const PNGraph& Graph, const int CtrNId);
+  int GetWthEdges() { return WthnEdges; }
+  int GetInEdges() { return InEdges; }
+  int GetOutEdges() { return OutEdges; }
   int GetCtrNId() { return CtrNId; }
 };
 
